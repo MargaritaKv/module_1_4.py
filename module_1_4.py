2bb0e2d0120356203a721e0fdfa0bee6f06e6e18
MargaritaKv <margarita269@yandex.ru>
2024-09-14 22:54:54 +0500

    module_1_4.py

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string='Valeria'
+print(len('Valeria'))
+print('my_string'.upper())
+print('my_string'.lower())
+my_string=my_string.replace(" ","")
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[-1])
