50c93ed42e07cb09f06169d9554bad6b19bf56ef
MargaritaKv <margarita269@yandex.ru>
2024-10-04 14:06:40 +0500

    Merge remote-tracking branch 'MargaritaKv/master'

---
+++ module_3_1.py
@@ -0,0 +1,11 @@
+calls = 0
+def count_calls ():
+    global callscalls
+    callscalls =+ 1
+def string (string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+def is_contains (string, list_to_seach):
+    count_calls()
+print(calls)
+
