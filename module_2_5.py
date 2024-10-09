fb152abe59fb5e28a64fb5b9a293820f1308759d
MargaritaKv <margarita269@yandex.ru>
2024-10-09 13:12:03 +0500

    Merge remote-tracking branch 'MargaritaKv/master'

---
+++ module_2_5.py
@@ -0,0 +1,11 @@
+n = int(input("Значения n = "))
+m = int(input("Значения m = "))
+value = int(input("Значения value = "))
+def get_matrix (n, m, value):
+    matrix()
+    for i in range (n):
+        get_matrix.append ('matrix')
+        for i in range (m):
+            get_matrix.append(value)
+        print (matrix)
+    return ("matrix")
